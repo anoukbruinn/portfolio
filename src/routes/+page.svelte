a719edcd7fcad679e795f1ea798fe1c2027a89dd
anoukbruinn <112856687+anoukbruinn@users.noreply.github.com>
2023-10-08 15:05:46 +0200

    done

---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<header>
+	<nav>
+		<h1>BRUIN</h1>
+		<ul>
+			<li><a href="#home">Home</a></li>
+			<li><a href="#Work">Work</a></li>
+			<li><a href="#contact">Contact</a></li>
+			<li><a href="#about">About</a></li>
+		</ul>
+	</nav>
+</header>
+<body />
+
+<style>
+	body {
+		background-color: rgb(33, 33, 33);
+	}
+
+	nav {
+	}
+
+	header {
+	}
+</style>
